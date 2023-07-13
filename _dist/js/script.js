1a3d0b4cb3cdae80c6b7ea2dba8ef4d4cebc22d3
NazarNinesquares <nazar@ninesquares.studio>
2023-07-13 16:32:26 +0300

    Змінив .gitignore

---
+++ _dist/js/script.js
@@ -0,0 +1,22 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./_src/js/modules/functions.js
+function starter() {
+	
+	console.log('Welcome to ninesquares gulp starter!');
+}
+;// CONCATENATED MODULE: ./_src/js/script.js
+
+
+var $ = jQuery;
+
+$(function() {
+
+	// custom scripts
+	starter()
+	
+})
+/******/ })()
+;
